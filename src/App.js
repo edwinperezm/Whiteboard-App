5bde2776bd1e65e855df245056449a6e5bb71d0c
edwinperezm <edwin@worksimpli.io>
2025-01-14 17:23:44 -0400

    removed comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Canvas } from './components/Canvas/CanvasRenderer.tsx';
 import { AppProvider } from './components/contexts/AppContext';
-// import { useAppStore } from './store/AppStore.ts';
+import { useAppStore } from './store/AppStore.ts';
 import './App.css';
 
 function App() {
